904b65015f9da45ceabdfe7483ff88ad6c50f372
aalmiray <aalmiray@gmail.com>
2011-12-17 20:38:20 +0100

    Refactor API controller by reducing duplication

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -1,17 +1,27 @@
 class UrlMappings {
     static mappings = {
         "/profile/$id"(controller: 'profile', action: 'show')
-        "/api/plugins/$name"(controller: 'api', action: 'pluginInfo')
+        '/api/plugins'(controller: 'api', action: 'list') {
+            type = 'plugin'
+        }
+        "/api/plugins/$name"(controller: 'api', action: 'info') {
+            type = 'plugin'
+        }
         "/api/plugins/$name/download/$version"(controller: 'api', action: 'download') {
             type = 'plugin'
         }
-        "/api/archetypes/$name"(controller: 'api', action: 'archetypeInfo')
+        '/api/archetypes'(controller: 'api', action: 'list') {
+            type = 'archetype'
+        }
+        "/api/archetypes/$name"(controller: 'api', action: 'info') {
+            type = 'archetype'
+        }
         "/api/archetypes/$name/download/$version"(controller: 'api', action: 'download') {
             type = 'archetype'
         }
         "/plugins"(controller: 'plugin', action: 'list')
-        name showPlugin: "/plugin/$name"(controller: 'plugin', action: 'show')
         "/archetypes"(controller: 'archetype', action: 'list')
+        name showPlugin: "/plugin/$name"(controller: 'plugin', action: 'show')
         name showArchetype: "/archetype/$name"(controller: 'archetype', action: 'show')
         "/release/show/$id"(controller: 'release', action: 'show')
         "/release/download/$id"(controller: 'release', action: 'download')
--- grails-app/controllers/griffon/portal/ApiController.groovy
+++ grails-app/controllers/griffon/portal/ApiController.groovy
@@ -5,60 +5,90 @@ import grails.util.GrailsNameUtils
 
 class ApiController {
     private final static String TIMESTAMP_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ"
+    private final static Map SORT_SETTINGS = [sort: 'artifactVersion', order: 'desc']
 
     static allowedMethods = [
-            plugins: 'GET',
-            archetypes: 'GET',
-            pluginInfo: 'GET',
-            archetypeInfo: 'GET']
+            list: 'GET',
+            info: 'GET',
+            download: 'GET']
 
-    def plugins() {
-        List list = Plugin.list(sort: 'name').collect([]) { Plugin plugin ->
-            asMap(plugin)
+    def list() {
+        List list = []
+        switch (params.type) {
+            case 'plugin':
+                list = Plugin.list(sort: 'name')
+                break
+            case 'archetype':
+                list = Archetype.list(sort: 'name')
+        }
+
+        list = list.collect([]) { artifact ->
+            asMap(artifact)
         }
 
         render list as JSON
     }
 
-    def archetypes() {
-        List list = Archetype.list(sort: 'name').collect([]) { Archetype archetype ->
-            asMap(archetype)
+    def info() {
+        Artifact artifact = null
+        switch (params.type) {
+            case 'plugin':
+                artifact = Plugin.findByName(params.name)
+                break
+            case 'archetype':
+                artifact = Archetype.findByName(params.name)
         }
 
-        render list as JSON
-    }
-
-    def pluginInfo() {
         Map data = [:]
-        Plugin plugin = Plugin.findByName(params.name)
-        if (plugin) {
-            data = asMap(plugin)
-            data.releases = Release.findAllByArtifact(plugin, [sort: 'artifactVersion', order: 'desc']).collect([]) {Release release ->
-                asMap(release)
+        if (artifact) {
+            data = asMap(artifact)
+            data.releases = []
+            Release.findAllByArtifact(artifact, SORT_SETTINGS).each {Release release ->
+                try {
+                    data.releases << asMap(release)
+                } catch (IOException ioe) {
+                    // ignore
+                }
             }
-        }
-        render data as JSON
-    }
-
-    def archetypeInfo() {
-        Map data = [:]
-        Archetype archetype = Archetype.findByName(params.name)
-        if (archetype) {
-            data = asMap(archetype)
-            data.releases = Release.findAllByArtifact(archetype, [sort: 'artifactVersion', order: 'desc']).collect([]) {Release release ->
-                asMap(release)
-            }
+        } else {
+            data = [
+                    action: 'info',
+                    response: 'Not Found',
+                    params: [
+                            type: params.type,
+                            name: params.name,
+                    ]
+            ]
         }
         render data as JSON
     }
 
     def download() {
+        Artifact artifact = null
+        switch (params.type) {
+            case 'plugin':
+                artifact = Plugin.findByName(params.name)
+                break
+            case 'archetype':
+                artifact = Archetype.findByName(params.name)
+        }
+        if (!artifact) {
+            render([
+                    action: 'download',
+                    response: 'Not Found',
+                    params: [
+                            type: params.type,
+                            name: params.name,
+                            version: params.version
+                    ]
+            ] as JSON)
+            return
+        }
+
         Release release = Release.withCriteria(uniqueResult: true) {
             and {
                 eq('artifactVersion', params.version)
-                artifact {
-                    eq('name', params.name)
-                }
+                eq('artifact', artifact)
             }
         }
         if (!release) {
